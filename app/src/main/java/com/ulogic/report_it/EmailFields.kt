19c582e0c0abe87a1ed29cad26bac7b2ab1af1cf
scotty9000 <34504938+scotty9000@users.noreply.github.com>
2022-07-24 15:21:01 +0100

    change package name and build release apk

---
+++ app/src/main/java/com/ulogic/report_it/EmailFields.kt
@@ -0,0 +1,19 @@
+package com.ulogic.report_it
+
+data class EmailFields(
+    var address1: String = "",
+    var subject1: String = "",
+    var body1: String = "",
+
+    var address2: String = "",
+    var subject2: String = "",
+    var body2: String = "",
+
+    var address3: String = "",
+    var subject3: String = "",
+    var body3: String = "",
+
+    var address4: String = "",
+    var subject4: String = "",
+    var body4: String = ""
+)

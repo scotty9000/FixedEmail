8a08f77eee79d595b70e4204a23eeaf66554c252
Martin Scott <scotty9000@gmail.com>
2024-03-01 12:37:18 +0000

    Move the button etc

--- app/src/main/java/com/ulogic/fixed_email/EmailFields.kt
+++ app/src/main/java/com/ulogic/fixed_email/EmailFields.kt
@@ -1,6 +1,8 @@
 package com.ulogic.fixed_email
 
 data class EmailFields(
+    val to: String = "To: ",
+    val subject: String = "Subject: ",
     var address1: String = "scotty9000@gmail.com",
     var subject1: String = "",
     var body1: String = ""
